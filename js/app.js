5cd09e222970eaeb5208ef3a32cdb87cb0d3f8f2
Siva kumar <101492425+Siva-kumar-r16@users.noreply.github.com>
2026-09-23 14:57:00 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -4,8 +4,7 @@
 ===================================================== */
 
 const API_BASE_URL =
-    "https://naturals-make-evaluate-optical.trycloudflare.com/";
-
+    "https://redhead-makeup-individually-convinced.trycloudflare.com";
 
 const PREDICT_URL =
     `${API_BASE_URL}/predict`;
