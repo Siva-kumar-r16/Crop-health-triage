51d7a248ff51fdf55d4e5fa74bbd99d190b39a31
Siva kumar <101492425+Siva-kumar-r16@users.noreply.github.com>
2026-09-23 14:54:57 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -4,7 +4,7 @@
 ===================================================== */
 
 const API_BASE_URL =
-    "https://naturals-make-evaluate-optical.trycloudflare.com";
+    "https://naturals-make-evaluate-optical.trycloudflare.com/";
 
 
 const PREDICT_URL =
